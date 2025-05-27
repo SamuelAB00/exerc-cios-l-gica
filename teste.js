9f6cb807b943feb2546c7eff752fee1cd66b3555
SamuelAB00 <samuel_ab_silva@estudante.sesisenai.org.br>
2025-05-27 17:08:08 -0300

    PRIMEIRO JAVASCRIPT

---
+++ teste.js
@@ -0,0 +1,10 @@
+// ler a idade do usuario
+// se idade 18 ou mais 
+//senão, diz que é menor 
+
+let idade = prompt ("Qual é sua idade?")
+if (idade >=18) {
+    alert ("você é maior")
+} else {
+    alert ("Você é manor de idade.")
+}
