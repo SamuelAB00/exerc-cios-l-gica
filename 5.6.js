2323d4e544308fc66d3fb7332240b7e5152ed6f3
SamuelAB00 <samuel_ab_silva@estudante.sesisenai.org.br>
2025-07-02 16:42:42 -0300

    atividade da pontuação do

---
+++ 5.6.js
@@ -0,0 +1,19 @@
+//5.6
+
+pontuação = Number (prompt ('Digite sua pontuação'))
+
+if (pontuação <=10) {
+    alert ("Deu ruim")
+}
+
+if (pontuação >10, pontuação <=100) {
+    alert ("tá... Você pode melhorar né seu bosta")
+}
+
+if (pontuação >100, pontuação <=200){
+    alert ("Supimpa!!")
+}
+
+if (pontuação >200) {
+    alert ("MITOU PORRA !!!")
+}
