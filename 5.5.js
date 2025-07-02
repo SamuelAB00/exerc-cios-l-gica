6b33497d89896fec6ae587691b5df994fa054e78
SamuelAB00 <samuel_ab_silva@estudante.sesisenai.org.br>
2025-07-02 16:20:27 -0300

    atividade do Mano Juca

---
+++ 5.5.js
@@ -0,0 +1,14 @@
+nota1 = Number(prompt('Qual a sua nota?'));
+
+nota2 = Number (prompt ('Agora escreva sua segunda nota'))
+
+notafinal = (nota1 + nota2) / 2
+
+if (notafinal >= 7.0) {
+    alert ("Parabéns mano Juca seu malandro você passou de ano")
+}else{
+    alert("Seu merdinha inútil, sua única função na vida é estudar")
+}
+
+
+
