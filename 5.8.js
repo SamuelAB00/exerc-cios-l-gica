d7140aec5318f533d744a370d4bb92e3bf109119
SamuelAB00 <samuel_ab_silva@estudante.sesisenai.org.br>
2025-07-07 16:19:46 -0300

    voto

---
+++ 5.8.js
@@ -0,0 +1,20 @@
+//5.8
+
+let idade = prompt("Qual é sua idade")
+
+if (idade < 16 ) {
+    alert ("Você não pode votar ainda")
+}
+
+if (idade >= 16 , idade <= 17) {
+    alert ("O seu voto é facultativo")
+}
+
+else if (idade >= 18 , idade <= 65) {
+    alert ("O seu voto é obrigatório")
+
+}
+
+if (idade > 65) {
+    alert ("O seu voto é facultativo")
+}    
